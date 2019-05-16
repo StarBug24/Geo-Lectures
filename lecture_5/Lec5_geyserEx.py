4801b1fdbd9a53590d8457fc3416fcd14eda2ea5
estherstern <e.hemmens@yahoo.de>
2019-05-16 15:59:41 +0200

    new exercise

--- lecture_5/Lec5_geyserEx.py
+++ lecture_5/Lec5_geyserEx.py
@@ -6,14 +6,24 @@
 # -----------------------------
 import Lec5_geyserEx_functions as h
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 # -- import the snuffler marker file, time in julian days --
-h.read_snuffler_marker
+julianday, eventclass= h.read_snuffler_marker("markerfile", length=0)
+julianday= np.sort(julianday)
+#plt.plot(julianday)
+#plt.show()
 
 
 # -- calculate the waiting time after eruption/ duration of bursts in minutes --  
+retime= []
+for i in range(1,len(julianday)):
+    retime.append((julianday[i]-julianday[i-1])*24*60)
 
+retime=np.diff(julianday)*24*60
+plt.plot(retime)
+plt.show
 
 
 # -- calculate mean time --
@@ -21,11 +31,11 @@ h.read_snuffler_marker
 
 
 # -- plot waiting time after eruptions/ duration of bursts vs. time --
-plot
+#plot
 
 
 # -- plot histogram of the intereruption times (1 h bins) -- 
-hist
+#hist
 
 
 
